e0fba27eea18e754cefceccdc0175fdf04019d01
“rouzbehmozafari” <rouzbeh.mozafari@yahoo.com>
2021-10-26 15:25:53 +0200

    first

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+let first = document.getElementById('1b');
+let sec = document.getElementById('2b');
+let third = document.getElementById('3b');
+let fourth = document.getElementById('4b');
+
+body = document.getElementById('bd');
+
+function CBG(color) {
+    bd.style.backgroundColor = color;
+};
+
